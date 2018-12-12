c70f4fa1fa3c90bfcd369f4624f2ce2abdd11fa9
Julie Park <jspark721@gmail.com>
2018-12-12 00:34:37 -0800

    add mobile sizes - make the site responsive

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,15 @@
 $(window).scroll(function(){
     $(".title").css("opacity", 1 - $(window).scrollTop() / 750);
 });
+
+/*hamburger menu*/
+
+let nav = document.querySelector('nav');
+let menu = document.getElementById('menu');
+
+function toggleMenu() {
+  menu.classList.toggle('open');
+  nav.classList.toggle('show');
+}
+
+menu.onclick = toggleMenu;
