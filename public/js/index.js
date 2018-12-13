93189953d7212cc457d4a7a6d2362e9b0233a7de
Julie Park <jspark721@gmail.com>
2018-12-12 16:07:10 -0800

    add jquery function for mobile vh

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(window).scroll(function(){
     $(".title").css("opacity", 1 - $(window).scrollTop() / 750);
 });
 
-/*hamburger menu*/
+// hamburger menu
 
 let nav = document.querySelector('nav');
 let menu = document.getElementById('menu');
@@ -13,3 +13,10 @@ function toggleMenu() {
 }
 
 menu.onclick = toggleMenu;
+
+// mobile viewport function
+
+if ($('body').hasClass('mobile')) {
+  var viewportHeight = $('.quote').outerHeight();
+  $('.quote').css({ height: window.innerHeight });
+}
