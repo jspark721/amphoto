5abc8e6482f412e9713c8da3ab30ad6e7da5c234
Julie Park <jspark721@gmail.com>
2019-01-28 12:19:52 -0800

    add wordpress theme development folder

---
+++ subtle-warmth theme/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+if( ! function_exists ('subtlewarmth_setup')) :
+
+    function subtlewarmth_setup() {
+      // let WordPress handle the title tags
+      add_theme_support( 'title-tag' );
+    }
+
+endif;
+add_action('after_setup_theme', 'subtlewarmth_setup');
+
+/* register menus */
+
+function register_subtlewarmth_menus() {
+    register_nav_menus(
+      array(
+        'primary' => __('Primary Menu')
+      )
+    );
+}
+add_action('init', 'register_subtlewarmth_menus');
+
+/* add stylesheets */
+
+function subtlewarmth_scripts() {
+    // enqueue the main stylesheet
+    wp_enqueue_style('subtlewarmth_styles', get_stylesheet_uri());
+    // enqueue google fonts Lora & Garamound
+    wp_enqueue_style('subtlewarmth_google_fonts', 'https://fonts.googleapis.com/css?family=Lora|Cormorant+Garamond');
+}
+
+add_action('wp_enqueue_scripts', 'subtlewarmth_scripts');
+
+function wpb_adding_scripts() {
+wp_register_script('my_amazing_script', get_template_directory_uri() . '/js/index.js', array('jquery'),'1.1', true);
+wp_enqueue_script('my_amazing_script');
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );
---
+++ subtle-warmth theme/index.php
@@ -0,0 +1,27 @@
+<?php
+/* Main Template File for displaying all pages by default */
+
+  get_header();
+?>
+
+<div class="gallery-page-container">
+  <div class="gallery-page-title">
+    <h1 class="cursive"><?php echo get_the_title(); ?></h1>
+  </div>
+  <div class="gallery-line"></div>
+  <a class="gallery-link" href="<?php bloginfo('url') ?>/gallery/">back to gallery</a>
+</div>
+
+    <?php
+      // Start the loop
+      if(have_posts()) {
+        while(have_posts()) {
+          the_post();
+          the_content();
+        }
+      }
+    ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
