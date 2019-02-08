b4f6bb6231e725b12e415485ac80fb6312167e45
Julie Park <jspark721@gmail.com>
2019-02-07 16:37:52 -0800

    add blog index template file - & fixed styles

---
+++ subtle-warmth theme/home.php
@@ -0,0 +1,65 @@
+<?php
+/* Template for Blog Index Page */
+
+  get_header();
+?>
+
+
+
+<div class="header">
+  <div class="header-blog">
+    <h1>the blog</h1>
+    <div class="header-category">
+      <h2>browse by category</h2>
+      <ul class="category-items">
+        <li><a>weddings</a></li>
+        <li><a>engagements</a></li>
+        <li><a>styled shoots</a></li>
+        <li><a>portraits</a></li>
+        <li><a>other</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="main-blog">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <div class="blog-posts">
+    <?php if (has_post_thumbnail()) : ?>
+    <div class="blog-date"><h4>jan 25</h4></div>
+    <div class="blog-img">
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    </div>
+  <?php else : ?>
+
+  <?php endif; ?>
+    <div class="blog-summary">
+      <div class="blog-title">
+        <h3><a href="<?php the_permalink(); ?>" class="category-title-link"><?php the_title(); ?></a></h3>
+        <div class="line-break"></div>
+      </div>
+      <div class="blog-content">
+        <p><?php the_excerpt(); ?> </p>
+        <button class="btn-main btn-sm btn-light"><a href="<?php the_permalink(); ?>">Read More</a></button>
+        <div class="blog-tags">
+          <p>filed in:<a><?php the_category('  ,  '); ?></a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="blog-break-line"></div>
+</div>
+<?php endwhile; ?>
+
+<?php else : ?>
+
+<article class="no-posts">
+
+    <h1>No posts were found.</h1>
+
+</article>
+
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
--- subtle-warmth theme/index.php
+++ subtle-warmth theme/index.php
@@ -1,46 +1,28 @@
 <?php
-/* Main Template File for displaying all pages by default */
+/*
+ Main Template File for displaying all blog posts by default */
 
   get_header();
 ?>
 
-<div class="header">
-  <div class="header-blog">
-    <h1>the blog</h1>
-    <div class="header-category">
-      <h2>browse by category</h2>
-      <ul class="category-items">
-        <li><a>weddings</a></li>
-        <li><a>engagements</a></li>
-        <li><a>styled shoots</a></li>
-        <li><a>portraits</a></li>
-        <li><a>other</a></li>
-      </ul>
-    </div>
+<div class="gallery-page-container">
+  <div class="gallery-page-title">
+    <h1 class="cursive"><?php echo get_the_title(); ?></h1>
   </div>
+  <div class="gallery-line"></div>
+  <a class="gallery-link" href="<?php bloginfo('url') ?>/blog/">back to index</a>
 </div>
 
-<div class="main-blog">
-  <div class="blog-posts">
-    <div class="blog-date"><h4>jan 25</h4></div>
-    <div class="blog-img">
-      <img src="<?php bloginfo('template_url') ?>/img/gallery1.jpg" />
-    </div>
-    <div class="blog-summary">
-      <div class="blog-title">
-        <h3><a>the blog title goes here</a></h3>
-        <div class="line-break"></div>
-      </div>
-      <div class="blog-content">
-        <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        <button class="btn-main btn-sm btn-light">read the post</button>
-        <div class="blog-tags">
-          <p>filed in:<a class="tags"> weddings</a></p>
-        </div>
-      </div>
-    </div>
+    <?php
+      // Start the loop
+      if(have_posts()) {
+        while(have_posts()) {
+          the_post();
+          the_content();
+        }
+      }
+    ?>
   </div>
-  <div class="blog-break-line"></div>
 </div>
 
 <?php get_footer(); ?>
