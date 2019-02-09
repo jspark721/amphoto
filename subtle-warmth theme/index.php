952d7d693441cbd51ffd7ec88392b567f795c091
Julie Park <jspark721@gmail.com>
2019-02-09 00:41:52 -0800

    add blog single post template

--- subtle-warmth theme/index.php
+++ subtle-warmth theme/index.php
@@ -5,23 +5,27 @@
   get_header();
 ?>
 
-<div class="gallery-page-container">
-  <div class="gallery-page-title">
-    <h1 class="cursive"><?php echo get_the_title(); ?></h1>
+<div class="post-signature"><span>arielminphotography blog</span></div>
+<div class="post-container">
+  <div class="post-category"><h4>weddings, styled shoot</h4></div>
+  <div class="post-title"><h1>the blog title goes here</h1></div>
+  <div class="post-date"><h4>feb 4th</h4></div>
+  <div class="post-vline"></div>
+  <div class="post-feature-img">
+    <img src="../public/img/gallery4.jpg" />
   </div>
-  <div class="gallery-line"></div>
-  <a class="gallery-link" href="<?php bloginfo('url') ?>/blog/">back to index</a>
-</div>
-
-    <?php
-      // Start the loop
-      if(have_posts()) {
-        while(have_posts()) {
-          the_post();
-          the_content();
-        }
-      }
-    ?>
+  <div class="post-break"></div>
+  <div class="post-content">
+    <p class="summary">Integer id lorem at libero dictum finibus at non lectus. Cras tristique vitae purus vitae porta. Integer sed dui eu massa imperdiet lacinia. Vivamus non lorem magna. Nunc lobortis in ante vel aliquet. Vestibulum justo elit, maximus sed nisl eget, consectetur convallis ex. Quisque ut nibh vitae metus bibendum ultricies et id neque.</p>
+    <p class="summary">Ut euismod, nisl sit amet volutpat cursus, justo velit viverra nulla, ut gravida nunc lectus vel libero. Etiam in bibendum massa. Nunc vitae bibendum ipsum. Phasellus molestie tristique diam at vulputate. Etiam fermentum, sapien at dignissim pretium, quam tortor feugiat augue, in viverra mi libero nec metus. Curabitur lobortis ex in maximus interdum. Morbi laoreet lorem eget neque consectetur auctor. Sed a leo vel diam finibus aliquam eu sit amet massa. Maecenas dignissim consectetur ex quis placerat. Nullam ac mattis erat.</p>
+  </div>
+  <div class="blog-tags">
+    <p>filed in:<a class="tags"> weddings</a></p>
+  </div>
+  <div class="post-nav">
+    <a class="btn-main btn-sm">« prev</a>
+    <a class="btn-main btn-sm btn-light">back to index</a>
+    <a class="btn-main btn-sm">next »</a>
   </div>
 </div>
 
